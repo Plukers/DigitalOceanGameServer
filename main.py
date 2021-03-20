0827a910e3ff1a3bb774736fd981f296f05a89cb
Lukas Prost <prost@posteo.de>
2021-03-20 20:32:40 +0100

    Create digital ocean droplet on start

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
 #!/usr/bin/env python
 
-# Copyright (C) 2003-2007  Robey Pointer <robeypointer@gmail.com>
-#
-# This file is part of paramiko.
-#
-# Paramiko is free software; you can redistribute it and/or modify it under the
-# terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation; either version 2.1 of the License, or (at your option)
-# any later version.
-#
-# Paramiko is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with Paramiko; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
-
-
 import base64
 import getpass
 import os
 import socket
 import getopt
 import sys
-import traceback
 from pathlib import Path
-from paramiko.py3compat import input
+import digitalocean
 
 import paramiko
+from paramiko.py3compat import input
 
 def exec_commands(commands):
     for command in commands:
@@ -42,14 +23,14 @@ def exec_commands(commands):
           print(stderr.read())
 
 def usage():
-  print("use it like: $python main.py -h localhost -s path/to/save")
+  print("use it like: $python main.py -s path/to/save")
 
 
 
 # Parse arguments
 try:
   options, remainder = getopt.gnu_getopt(
-    sys.argv[1:], 'h:s:v', ["host=", "save=", "verbose", "version", "help", "stop",])
+    sys.argv[1:], 'h:s:v', ["stop", "save=", "verbose", "version", "help"])
 except getopt.GetoptError as err:
   print("*** " + str(err))
   usage()
@@ -58,32 +39,23 @@ except getopt.GetoptError as err:
 version = "0.5"
 verbose = False
 port = 22
-username = ""
-hostname = ""
 save_file_path = ""
-stopping_server = False
+stop_server = False
 
 for opt, arg in options:
-  if opt in ("-h", "--host"):
-    hostname = arg
-    if hostname.find("@") >= 0:
-      username, hostname = hostname.split("@")
-    else:
-      print("*** Hostname required.")
-      sys.exit(1)
-  elif opt in ("-s", "--save"):
+  if opt in ("-s", "--save"):
     save_file_path = arg
     if(not os.path.exists(save_file_path)):
       print("*** Save file not found!")
       sys.exit(1)
-  elif opt in ("-v", "--verbose"):
+  elif opt in ("--verbose"):
     verbose = True
-  elif opt in ("--version"):
+  elif opt in ("-v", "--version"):
     print(str(sys.argv[0]) + " version: " + str(version))
     sys.exit(0)
   elif opt in ("--stop"):
-    stopping_server = True
-  elif opt in ("--help"):
+    stop_server = True
+  elif opt in ("-h", "--help"):
     print("print help here!")
     sys.exit(0)
 
@@ -91,10 +63,8 @@ verboseprint = print if verbose else lambda *a, **k: None
 verboseprint("OPTIONS    :", options)
 verboseprint("ARGV       :", sys.argv[1:])
 
-
-
-if(not hostname or not save_file_path):
-  print("*** user@hostname and path to save file required.")
+if(not save_file_path):
+  print("*** Path to save file required.")
   sys.exit(1)
 
 # get rsa key
@@ -108,54 +78,97 @@ if(not pw):
   pw = None
 pkey = paramiko.Ed25519Key.from_private_key_file(str(ssh_key_path), password=pw)
 
+
+print("*** Creating droplet...")
+
+token = "token here"
+
+manager = digitalocean.Manager(token=token)
+keys = manager.get_all_sshkeys()
+
+droplet = digitalocean.Droplet(token=token,
+                                name='FactorioServer',
+                                region='fra1',
+                                image='docker-20-04', 
+                                size_slug='s-1vcpu-1gb',
+                                ssh_keys=keys, #Add all keys
+                                backups=False)
+droplet.create()
+
+droplet_created = False
+while not droplet_created:
+  for action in droplet.get_actions():
+      action.load()
+      if action.status == "completed":
+        droplet_created = True;
+        break
+      if action.status == "errored":
+        print("Error when creating droplet.")
+        sys.exit(1)
+
+# Request all droplets and find recently created droplet
+# We do this as the created droplet object is missing some information, e.g. ip address
+found_droplet = False
+my_droplets = manager.get_all_droplets()
+for i, v in enumerate(my_droplets):
+  if v.id == droplet.id:
+      droplet = v
+      found_droplet = True;
+
+if(not found_droplet):
+  print("*** Could not setup droplet.")
+  sys.exit(1)
+
+username = "root"
+hostname = droplet.ip_address
+
 # now, connect and use paramiko Client to negotiate SSH2 across the connection
 try:
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.WarningPolicy())
-    print("*** Connecting...")
-    try:
-        client.connect(
-            hostname,
-            port,
-            username,
-            pkey=pkey,
-        )
-    except Exception:
-        pw = getpass.getpass("Password for private key wrong?: " % (username, hostname))
-        pkey = paramiko.Ed25519Key.from_private_key_file(str(ssh_key_path), password=pw)
-        client.connect(
-            hostname,
-            port,
-            username,
-            pkey=pkey,
-        )
+  client = paramiko.SSHClient()
+  client.load_system_host_keys()
+  client.set_missing_host_key_policy(paramiko.WarningPolicy())
+  print("*** Connecting...")
+  try:
+      client.connect(
+          hostname,
+          port,
+          username,
+          pkey=pkey,
+      )
+  except Exception:
+      pw = getpass.getpass("Password for private key wrong?: ")
+      pkey = paramiko.Ed25519Key.from_private_key_file(str(ssh_key_path), password=pw)
+      client.connect(
+          hostname,
+          port,
+          username,
+          pkey=pkey,
+      )
 
-    commands = [ 
-      "sudo mkdir -p /opt/factorio", 
-      "sudo chown 845:845 /opt/factorio",
-      "sudo docker run -d -p 34197:34197/udp -p 27015:27015/tcp -v /opt/factorio:/factorio --name factorio --restart=always factoriotools/factorio",
-      "docker stop factorio",
-    ]
-    exec_commands(commands)
+  commands = [ 
+    "sudo mkdir -p /opt/factorio", 
+    "sudo chown 845:845 /opt/factorio",
+    "sudo docker run -d -p 34197:34197/udp -p 27015:27015/tcp -v /opt/factorio:/factorio --name factorio --restart=always factoriotools/factorio",
+    "docker stop factorio",
+  ]
+  exec_commands(commands)
 
-    sftp = client.open_sftp()
+  sftp = client.open_sftp()
 
-    print("Copying save file {}".format(save_file_path))
-    sftp.put(save_file_path, os.path.join("/opt/factorio/saves", os.path.basename(save_file_path)))
+  print("Copying save file {}".format(save_file_path))
+  sftp.put(save_file_path, os.path.join("/opt/factorio/saves", os.path.basename(save_file_path)))
 
-    sftp.close()
+  sftp.close()
 
-    commands = [
-      "docker start factorio",
-    ]
-    exec_commands(commands)
-   
-    client.close()
+  commands = [
+    "docker start factorio",
+  ]
+  exec_commands(commands)
+  
+  client.close()
 
 except Exception as e:
     print("*** Caught exception: %s: %s" % (e.__class__, e))
-    # traceback.print_exc()
     try:
         client.close()
         pass
