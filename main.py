dfbed2cce7e0604274fab77d51600c3733d6bc43
joschi1212 <e1427350@student.tuwien.ac.at>
2021-03-20 13:39:41 +0100

    add parameter parsing with getopt

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ import base64
 import getpass
 import os
 import socket
+import getopt
 import sys
 import traceback
 from pathlib import Path
@@ -39,25 +40,56 @@ def exec_commands(commands):
         if errors:
           print("*** Errors")
           print(stderr.read())
-    
 
+
+
+
+# Parse arguments
+
+options, remainder = getopt.gnu_getopt(
+  sys.argv[1:], 'h:s:v', ["host=", "save=", "verbose", "version", "help", "stop",])
+
+
+version = "0.5"
+verbose = False
 port = 22
-
-if len(sys.argv) != 3:
-    print("*** user@hostname and path to save file required.")
-    sys.exit(1)
-
 username = ""
-hostname = sys.argv[1]
-if hostname.find("@") >= 0:
-  username, hostname = hostname.split("@")
-else:
-  print("*** Hostname required.")
-  sys.exit(1)
+hostname = ""
+save_file_path = ""
+stopping_server = False
 
-save_file_path = sys.argv[2]
-if(not os.path.exists(save_file_path)):
-  print("*** Save file not found.")
+for opt, arg in options:
+  if opt in ("-h", "--host"):
+    hostname = arg
+    if hostname.find("@") >= 0:
+      username, hostname = hostname.split("@")
+    else:
+      print("*** Hostname required.")
+      sys.exit(1)
+  elif opt in ("-s", "--save"):
+    save_file_path = arg
+    if(not os.path.exists(save_file_path)):
+      print("*** Save file not found!")
+      sys.exit(1)
+  elif opt in ("-v", "--verbose"):
+    verbose = True
+  elif opt in ("--version"):
+    print(str(sys.argv[0]) + " version: " + str(version))
+    sys.exit(0)
+  elif opt in ("--stop"):
+    stopping_server = True
+  elif opt in ("--help"):
+    print("print help here!")
+    sys.exit(0)
+
+verboseprint = print if verbose else lambda *a, **k: None
+verboseprint("OPTIONS    :", options)
+verboseprint("ARGV       :", sys.argv[1:])
+
+
+
+if(not hostname or not save_file_path):
+  print("*** user@hostname and path to save file required.")
   sys.exit(1)
 
 # get rsa key
