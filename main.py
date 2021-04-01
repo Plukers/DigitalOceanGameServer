3abb55a9bbd0dd950bc3c419f07c5fb9744ca756
Lukas Prost <prost@posteo.de>
2021-04-02 00:06:39 +0200

    Improved functionality

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import socket
 import getopt
 import sys
+from binascii import hexlify
 from pathlib import Path
 import digitalocean
 
@@ -15,12 +16,12 @@ from paramiko.py3compat import input
 def exec_commands(commands):
     for command in commands:
         print("*** {}".format(command))
-        stdin, stdout, stderr = client.exec_command(command)
+        _, stdout, stderr = client.exec_command(command)
         print(stdout.read().decode())
         errors = stderr.read().decode()
         if errors:
           print("*** Errors")
-          print(stderr.read())
+          print(errors)
 
 def usage():
   f = open("help.txt", "r")
@@ -31,7 +32,7 @@ def usage():
 # Parse arguments
 try:
   options, remainder = getopt.gnu_getopt(
-    sys.argv[1:], 'h:s:v', ["stop", "save=", "verbose", "version", "help"])
+    sys.argv[1:], 'h:s:v:t', ["stop", "save=", "verbose", "token=", "version", "help"])
 except getopt.GetoptError as err:
   print("*** " + str(err))
   usage()
@@ -42,6 +43,7 @@ verbose = False
 port = 22
 save_file_path = ""
 stop_server = False
+token = ""
 
 for opt, arg in options:
   if opt in ("-s", "--save"):
@@ -49,6 +51,8 @@ for opt, arg in options:
     if(not os.path.exists(save_file_path)):
       print("*** Save file not found!")
       sys.exit(1)
+  elif opt in ("-t", "--token"):
+    token = arg
   elif opt in ("--verbose"):
     verbose = True
   elif opt in ("-v", "--version"):
@@ -64,32 +68,42 @@ verboseprint = print if verbose else lambda *a, **k: None
 verboseprint("OPTIONS    :", options)
 verboseprint("ARGV       :", sys.argv[1:])
 
-if(not save_file_path):
-  print("*** No save file provided. New save file will be created.")
-
 # get rsa key
 ssh_key_path = os.path.join(Path.home(), ".ssh", "id_ed25519")
 if(not os.path.exists(ssh_key_path)):
   print("No .ssh/id_ed25519 key found for server login.")
   sys.exit(1)
 
-pw = getpass.getpass("Password for key .ssh/id_ed25519:")
-if(not pw):
-  pw = None
-pkey = paramiko.Ed25519Key.from_private_key_file(str(ssh_key_path), password=pw)
-
-token = "token here"
+try:
+  pkey = paramiko.Ed25519Key.from_private_key_file(str(ssh_key_path))
+except Exception:
+  pw = getpass.getpass("Password for key .ssh/id_ed25519:")
+  if(not pw):
+    pw = None
+  pkey = paramiko.Ed25519Key.from_private_key_file(str(ssh_key_path), password=pw)
 
 if (stop_server):
-  print("*** Shutdown droplet...")
+  print("*** Destroying Factorio servers...")
 
   manager = digitalocean.Manager(token=token)
 
+  numDestroyed = 0
+
   my_droplets = manager.get_all_droplets()
   for i, v in enumerate(my_droplets):
+    if(v.name == 'FactorioServer'):
       v.destroy()
+      numDestroyed += 1
 
+  pluralS = ""
+  if (numDestroyed != 1):
+    pluralS = "s"
+
+  print("*** {} Factorio server{} destroyed".format(numDestroyed, pluralS))
   sys.exit(0)
+
+if(not save_file_path):
+  print("*** No save file provided. New save file will be created.")
 
 
 print("*** Creating droplet...")
@@ -111,7 +125,7 @@ while not droplet_created:
   for action in droplet.get_actions():
       action.load()
       if action.status == "completed":
-        droplet_created = True;
+        droplet_created = True
         break
       if action.status == "errored":
         print("Error when creating droplet.")
@@ -124,7 +138,7 @@ my_droplets = manager.get_all_droplets()
 for i, v in enumerate(my_droplets):
   if v.id == droplet.id:
       droplet = v
-      found_droplet = True;
+      found_droplet = True
 
 if(not found_droplet):
   print("*** Could not setup droplet.")
@@ -133,11 +147,22 @@ if(not found_droplet):
 username = "root"
 hostname = droplet.ip_address
 
+class IgnorePolicy(paramiko.MissingHostKeyPolicy):
+  """
+  Policy for logging a Python-style warning for an unknown host key, but
+  accepting it. This is used by `.SSHClient`.
+  """
+
+  def missing_host_key(self, client, hostname, key):
+    print("*** Unknown {} host key for {}: {}".format(
+              key.get_name(), hostname, hexlify(key.get_fingerprint()).decode()
+          ))
+
 # now, connect and use paramiko Client to negotiate SSH2 across the connection
 try:
   client = paramiko.SSHClient()
   client.load_system_host_keys()
-  client.set_missing_host_key_policy(paramiko.WarningPolicy())
+  client.set_missing_host_key_policy(IgnorePolicy())
   print("*** Connecting...")
   try:
       client.connect(
@@ -146,7 +171,7 @@ try:
           username,
           pkey=pkey,
       )
-  except Exception:
+  except Exception as e:
       pw = getpass.getpass("Password for private key wrong?: ")
       pkey = paramiko.Ed25519Key.from_private_key_file(str(ssh_key_path), password=pw)
       client.connect(
@@ -160,11 +185,16 @@ try:
     "sudo mkdir -p /opt/factorio", 
     "sudo chown 845:845 /opt/factorio",
     "sudo docker run -d -p 34197:34197/udp -p 27015:27015/tcp -v /opt/factorio:/factorio --name factorio --restart=always factoriotools/factorio",
-    "docker stop factorio",
   ]
   exec_commands(commands)
 
   if(save_file_path):
+    commands = [
+      "docker stop factorio",
+      "rm /opt/factorio/saves/*"
+    ]
+    exec_commands(commands)
+
     sftp = client.open_sftp()
 
     print("Copying save file {}".format(save_file_path))
@@ -172,14 +202,14 @@ try:
 
     sftp.close()
 
-  commands = [
-    "docker start factorio",
-  ]
-  exec_commands(commands)
+    commands = [
+      "docker start factorio",
+    ]
+    exec_commands(commands)
   
   client.close()
 
-  print("Server id: {}".format(hostname))
+  print("*** Server address: {}:34197".format(hostname))
 
 except Exception as e:
     print("*** Caught exception: %s: %s" % (e.__class__, e))
